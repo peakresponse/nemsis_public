574fca61a8d32a51467debd83491b71f453cc2e0
jrojas <Jorge.Rojas@hsc.utah.edu>
2015-10-05 15:56:28 -0600

    add data dictionary and API for State Data Set

---
+++ DataDictionary/PDFHTML/STATE/js/main.js
@@ -0,0 +1,11 @@
+.f1, body, td { font-family: "Arial", sans-serif; font-style: normal; font-weight: normal; text-decoration: none; color: black }
+.f2 { font-weight: bold }
+.f2-1 { font-size: 14px; font-weight: bold }
+.f3 { font-size: 12; font-weight: bold }
+.f4 { font-size: 12 }
+.f4_1 { font-size: 12; color:grey;}
+.f5 { font-family: "Verdana", sans-serif; font-size: 10; color: #B2B2B2 }
+.f6 { font-family: "Verdana", sans-serif; font-size: 10; text-decoration: underline; color: #B2B2B2 }
+.p1 { white-space:nowrap }
+.p2 { margin-top:2pt; white-space:nowrap }
+.p3 { margin-top:5pt; white-space:nowrap }
